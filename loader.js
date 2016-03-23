43d7fbc03667f282e188a8656bf3b241064d6ef3
WiBla <contact.wibla@gmail.com>
2016-03-23 21:25:54 +0100

    Published code
    I'd like to mention that icons are temporary

---
+++ loader.js
@@ -0,0 +1,37 @@
+(function(){
+  function $(selector){return document.querySelectorAll(selector);}
+  function loadScript(URL) {
+    // if not completely loaded, call again until loaded
+    if (!$("#app-menu .list .logout div span").length) {
+      if (typeof URL !== "undefined") setTimeout(function(){loadScript(URL)}, 200);
+      else setTimeout(loadScript, 200);
+    }
+    else {
+      var script = document.createElement("script");
+      script.type = "text/javascript";
+      if (typeof URL !== "undefined") script.src = URL;
+      else script.src = "https://rawgit.com/WiBla/Script/master/ressources/WiBla.js";
+      $("head")[0].appendChild(script);
+    }
+  }
+  function autoReload() {
+    // if not completely loaded, call again until loaded
+    if (!$("#video").length) setTimeout(loadScript, 200);
+    // Remove annoying video, we all have seen them enough
+    if ($("#video").length) $(".content")[0].removeChild($("#video")[0]);
+    if (!$(".loading-box").length) setTimeout(function(){location.reload();}, 60*1000);
+  }
+
+  // Retrieve options
+  chrome.storage.sync.get({
+    // Default settings
+    enabled: true,
+    autof5: true,
+    custom_enabled: false,
+    custom_URL: ""
+  }, function(items) {
+    if (items.enabled) loadScript();
+    if (items.autof5) autoReload();
+    if (items.custom_enabled) loadScript(items.custom_URL);
+  });
+})();
---
+++ options.js
@@ -0,0 +1,37 @@
+// Saves options to chrome.storage.sync.
+function save_options() {
+  var enabled = document.getElementById('enabled').checked;
+  var reload =  document.getElementById('reload').checked;
+  var custom_enabled =  document.getElementById('custom-enabled').checked;
+  var custom_URL =  document.getElementById('URL').value;
+  chrome.storage.sync.set({
+    enabled: enabled,
+    autof5: reload,
+    custom_enabled: custom_enabled,
+    custom_URL: custom_URL
+  }, function() {
+    // Update save button to let user know options were saved.
+    var button = document.getElementById('save');
+    button.textContent = 'Options saved !';
+    setTimeout(function(){button.textContent = 'Save';}, 2000);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value enabled and autof5 = true.
+  chrome.storage.sync.get({
+    enabled: true,
+    autof5: true,
+    custom_enabled: false,
+    custom_URL: ".js URL (https)"
+  }, function(items) {
+    document.getElementById('enabled').checked = items.enabled;
+    document.getElementById('reload').checked = items.autof5;
+    document.getElementById('custom-enabled').checked = items.custom_enabled;
+    document.getElementById('URL').value = items.custom_URL;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
